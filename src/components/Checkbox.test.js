586379237bdd821b2e75a1a1725d325d2aef52c5
Asher <asher.barak@gmail.com>
2017-12-24 14:27:35 +0200

    Enzime DOM testing

--- src/components/Checkbox.test.js
+++ src/components/Checkbox.test.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Checkbox from './Checkbox'
 import renderer from 'react-test-renderer';
+import {shallow} from 'enzyme';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
@@ -15,4 +20,20 @@ it('Renders consistent tree', () => {
     );
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+// DOM using enzime
+it('Reacts renders box and content', () => {
+    const component = shallow(
+      <Checkbox>test</Checkbox>,
+    );
+    expect(component.text()).toEqual(' ☐ test');
+  });
+  
+  it('Reacts to click by UI change', () => {
+    const component = shallow(
+      <Checkbox>test</Checkbox>,
+    );
+    component.find('.icon').simulate('click');
+    expect(component.text()).toEqual(' ☑ test');
   });
