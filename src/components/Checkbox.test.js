39d29bc8543d2544f6824bd3975a02dc13070e40
Asher <asher.barak@gmail.com>
2017-12-24 14:24:13 +0200

    Snapshot test

--- src/components/Checkbox.test.js
+++ src/components/Checkbox.test.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Checkbox from './Checkbox'
+import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Checkbox />, div);
+  });
+
+// Snapshot
+it('Renders consistent tree', () => {
+    const component = renderer.create(
+      <Checkbox />,
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
   });
